2af1e8c8c05a0143f312a1a69c3394d03c177705
Deepak-29-y <85682413+Deepak-29-y@users.noreply.github.com>
2022-03-02 13:19:27 +0530

    First commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {RiMenu3Line,RiCloseLin} from "react-icons"
+import "./navbar.css"
+
+function Navbar() {
+  return (
+    <div className="gpt3__navbar">Navbar</div>
+  )
+}
+
+export default Navbar
